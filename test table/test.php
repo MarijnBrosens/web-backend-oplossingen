33e2df016e5a1ba9e232b897f88e31d768cc88ef
Marijn Brosens <marijnbrosens16@hotmail.com>
2014-09-30 10:24:18 +0200

    opdracht comments

---
+++ test table/test.php
@@ -0,0 +1,19 @@
+<?php 
+
+	$aantalRijen 	= 2;
+	$aantalKolommen = 4;
+	$rijenTeller	= 0;
+
+ ?>
+
+
+<table>
+
+<?php while ($rijenTeller<$aantalRijen): ?>
+
+	<?="test" ?>
+	<?php ++$rijenTeller?>
+
+<?php endwhile  ?>
+
+</tabe>
