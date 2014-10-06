56109cdc7df6a17fee9aee05742759376a22be95
Marijn Brosens <marijnbrosens16@hotmail.com>
2014-10-06 22:24:22 +0200

    oplossing opdracht-functies-gevroderd

--- opdracht-functies-gevorderd/opdracht-functies-gevorderd.php
+++ opdracht-functies-gevorderd/opdracht-functies-gevorderd.php
@@ -1,9 +1,40 @@
 <?php 
 
-	function berekensom($getal1,$getal2){
-		$resultaat	=	$getal1	+	$getal2;
-		return $resultaat;
+	$md5HashKey 	= 	'd1fa402db91a7a93c4f414b8278ce073';
+	$karakter1		=	"2";
+	$karakter2		=	"8";
+	$karakter3		=	"a";
+
+	function functie1( $haystack, $karakter )
+	{
+		$haystackCount =  strlen( $haystack );
+		$karakterAantal = substr_count ( $haystack, $karakter );
+		
+		return $karakterAantal;
 	}
+
+	function functie2( $karakter )
+	{
+		global $md5HashKey;
+		$haystack = $md5HashKey;
+		$haystackCount =  strlen( $haystack );
+		$karakterAantal = substr_count ( $haystack, $karakter );
+
+		return $karakterAantal;
+	}
+
+	function functie3( $karakter )
+	{
+		$haystack = $GLOBALS['md5HashKey'];
+		$haystackCount =  strlen( $haystack );
+		$karakterAantal = substr_count ( $haystack, $karakter );
+
+		return $karakterAantal;
+	}
+
+	$eersteMethode 	=	functie1( $md5HashKey, $karakter1 );
+	$tweedeMethode 	=	functie2( $karakter2 );
+	$derdeMethode 	=	functie3( $karakter3 );
 
  ?>
 
@@ -16,23 +47,11 @@
     <body>
 	
 	<h1>Opdracht functies-gevorderd</h1>
-	<pre>
-	</pre>
-	<p>
-		<?=($som) ?>
-	</p>
-	<p>
-		<?=($vermenigvuldiging) ?>
-	</p>		
-		<?php if ( $boolEven ): ?>
-			<p>true</p>
-		<?php else: ?>
-			<p>false</p>
-		<?php endif ?>
+		<pre>
 
-	<p>
-		<?php echo $upperLenght['upper'] ?> is 
-		<?php echo $upperLenght['length'] ?> karakters lang.
-	</p>	
+		</pre>
+		<p> Functie1: De needle '<?php echo $karakter1?>' komt <?php echo $eersteMethode ?> keer voor in de hash '<?php echo $md5HashKey ?>'</p>
+		<p> Functie2: De needle '<?php echo $karakter2?>' komt <?php echo $tweedeMethode ?> keer voor in de hash '<?php echo $md5HashKey ?>'</p>
+		<p> Functie3: De needle '<?php echo $karakter3?>' komt <?php echo $derdeMethode ?> keer voor in de hash '<?php echo $md5HashKey ?>'</p>	
     </body>
 </html>
