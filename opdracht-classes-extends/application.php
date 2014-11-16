d2d3595fa473fdb9b170333651fe97f8eeab35d2
Marijn Brosens <marijnbrosens16@hotmail.com>
2014-11-17 00:51:02 +0100

    opdracht-classes-extends-Stap2

--- opdracht-classes-extends/application.php
+++ opdracht-classes-extends/application.php
@@ -4,12 +4,49 @@
         include 'classes/' . $className . '.php';
     }
 
-    $meerkat    = new Animal('Timon', 'male', 50);
+    $meerkat    = new Animal('Timon', 'almost female', 50);
     $boar    	= new Animal('Poomba', 'male', 70);
     $bird   	= new Animal('Zazu', 'male', 30);
 
     $meerkat->changeHealth(-20);
-    $boar->changeHealth(-10);
+    $boar->changeHealth(+10);
     $bird->changeHealth(-40);
 
 ?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>opdracht-classes-extends</title>
+        <link rel="stylesheet" href="../style.css">
+    </head>
+    <body>
+
+        <h1>Opdracht classes extends</h1>
+
+        <h2><?php echo $meerkat->getName(); ?></h2>
+        <ul>
+            <li><?php echo $meerkat->getGender(); ?></li>
+            <li><?php echo $meerkat->getHealth(); ?></li>
+            <li><?php echo $meerkat->doSpecialMove(); ?></li>
+        </ul>
+
+        <h2><?php echo $boar->getName(); ?></h2>
+        <ul>
+            <li><?php echo $boar->getGender(); ?></li>
+            <li><?php echo $boar->getHealth(); ?></li>
+            <li><?php echo $boar->doSpecialMove(); ?></li>
+        </ul>
+
+        <h2><?php echo $bird->getName(); ?></h2>
+        <ul>
+            <li><?php echo $bird->getGender(); ?></li>
+            <li><?php echo $bird->getHealth(); ?></li>
+            <li><?php echo $bird->doSpecialMove(); ?></li>
+        </ul>
+
+    </body>
+</html>
