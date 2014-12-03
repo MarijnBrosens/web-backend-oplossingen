c0787ce97f9279f20fab4195eb0fb4c5ef388466
Marijn Brosens <marijnbrosens16@hotmail.com>
2014-12-03 23:42:21 +0100

    opdracht-CRUD-delete

---
+++ opdracht-CRUD-delete-b/body.view.php
@@ -0,0 +1,51 @@
+
+	<form action="<?= $_SERVER['PHP_SELF'] ?>" method="POST">
+		<table>
+			
+			<thead>
+				<tr>
+					<th>#</th>
+					<?php foreach ($brouwersFieldnames as $fieldname): ?>
+						<th><?= $fieldname ?></th>
+					<?php endforeach ?>
+					<th></th>
+				</tr>
+			</thead>
+
+			<?php if ( $deleteConfirm ): ?>
+				<div>
+					Bent u zeker dat u deze record wilt verwijderen?
+					<form action="<?= $_SERVER['PHP_SELF'] ?>" method="POST">
+
+						<button type="submit" name="delete" value="<?= $deleteId ?>">
+							Absoluut zeker!
+						</button>
+
+						<button type="submit">
+							Ongedaan maken
+						</button>
+
+					</form>
+				</div>
+			<?php endif ?>			
+
+			<tbody>
+				<?php foreach ($brouwers as $key => $brouwer): ?>
+					<tr class="<?= ( $brouwer['brouwernr'] === $deleteId ) ? 'confirm-delete' : ''  ?>">
+						<td><?= ++$key ?></td>
+						<?php foreach ($brouwer as $value): ?>
+							<td><?= $value ?></td>
+						<?php endforeach ?>
+						<td>
+							<!-- http://stackoverflow.com/questions/7935456/input-type-image-submit-form-value -->
+							<button type="submit" name="confirm" value="<?= $brouwer['brouwernr'] ?>" class="btn">
+								<img src="delete-icon-mini.png" alt="Delete button">Delete
+							</button>
+						</td>
+					</tr>
+				<?php endforeach ?>
+				
+			</tbody>
+
+		</table>
+	</form>
---
+++ opdracht-CRUD-delete-b/index.php
@@ -0,0 +1,55 @@
+<?php
+
+	$geselecteerdeBrouwer	=	false;
+	$deleteConfirm			=	false;
+	$deleteId				=	false;
+
+	//opdracht-Security-login bekijken
+	spl_autoload_register(  function( $class ) { include_once( $class .'.php' ); } );
+
+	function view( $file, $data = false )
+	{
+		if ( $data )
+		{
+			extract( $data );
+		} 
+
+		include( $file );
+	}
+
+
+	try {
+
+		if ( isset( $_POST[ 'confirm' ] ) )
+		{
+			$deleteConfirm	=	true;
+			$deleteId		=	$_POST[ 'confirm' ];
+		}
+
+		$connection	=	new PDO( 'mysql:host=localhost;dbname=bieren', 'root', '' );
+		$db 		= 	new Database( $connection );
+		$db->deleteQuery( '	DELETE FROM brouwers
+							WHERE brouwernr = :brouwernr');
+
+		$dataArray = $db->selectQuery('	SELECT * 
+							FROM brouwers');
+
+		$brouwersFieldnames = $dataArray['brouwersFieldnames'];
+		$brouwers 			= $dataArray['brouwers'];
+
+	} catch ( PDOException $e ) {
+
+		Message::setMessage( $e->getMessage() , 'error');
+	}
+
+	view( 'header.view.php', array( 'title' 	=> 'Opdracht-CRUD-delete', 
+									'messages' 	=> Message::getMessages() ) );
+
+	view( 'body.view.php', array(	'brouwersFieldnames'=> $brouwersFieldnames,
+									'brouwers' 			=> $brouwers,
+									'deleteConfirm'		=> $deleteConfirm,
+									'deleteId'			=> $deleteId) );
+
+	view( 'footer.view.php' );
+
+?>
