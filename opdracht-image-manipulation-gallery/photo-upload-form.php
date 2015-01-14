d916710efa67faef8e5cc268a52004f6bfb916f6
Marijn Brosens <marijnbrosens16@hotmail.com>
2015-01-14 23:54:02 +0100

    opdracht-image-manipulation-gallery

---
+++ opdracht-image-manipulation-gallery/photo-upload-form.php
@@ -0,0 +1,55 @@
+<?php
+
+    session_start();
+
+    function __autoload( $className ) {
+
+        include_once( 'classes/' . $className . '.php' );
+
+    }
+
+    $message = Message::getMessage();
+
+?>
+
+<!doctype html>
+<html>
+<head>
+
+    <title>Opdracht image manipulation gallery</title>
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../style.css">
+
+</head>
+<body>
+
+    <?php if ( isset ( $message ) ): ?>
+
+        <div class="modal <?= $message['type'] ?>">
+            <?= $message['text'] ?>
+        </div>
+        
+    <?php endif ?>  
+
+    <ul>
+        <li><a href="gallery.php"><i class="fa fa-arrow-left"></i> Terug naar gallery</a></li>
+    </ul>
+
+    <h1>Foto uploaden</h1>
+
+    <form action="photo-upload.php" method="POST" enctype="multipart/form-data">
+
+        <input type="file" name="image" id="image">
+
+        <label for="titel">Titel</label>
+        <input type="text" name="titel" placeholder="Titel">
+
+        <label for="beschrijving">Beschrijving</label>
+        <input type="text" name="beschrijving" placeholder="Beschrijving">
+
+        <input type="submit" name="submit" value="Verzenden">
+
+    </form>
+
+</body>
+</html>
---
+++ opdracht-image-manipulation-gallery/photo-upload.php
@@ -0,0 +1,83 @@
+<?php
+
+	session_start();
+
+	function __autoload( $classname )
+	{
+		include_once( 'classes/' . $classname . '.php' );
+	}
+
+	var_dump( $_POST );
+	var_dump( $_FILES );
+
+	if ( isset( $_POST[ 'submit' ] ) )
+	{
+
+		$image 	=	new Image( 	$_FILES['image']['name'], 
+								$_FILES['image']['type'], 
+								$_FILES['image']['tmp_name'], 
+								$_FILES['image']['error'],
+								$_FILES['image']['size'] );
+
+		$isType = 	$image->validateType( array( 	"image/jpeg", 
+													"image/png", 
+													"image/gif" ) );
+
+		$isSize	=	$image->validateSize( 2000000 ); // 2MB
+
+		$hasError	=	$image->validateError( );
+
+
+		if ( $isType && $isSize && !$hasError )
+		{
+			$isUploaded = $image->upload( '../uploads/img/bin' );
+
+
+	        if( !$isDuplicate ) {
+
+	            $isUploaded = $image->upload( '../uploads/img/' );
+
+	        }
+
+	        if( $isUploaded ) {
+
+	            $hasThumbnail = $image->create_thumbnail( 50, 50 );
+
+	            if ( $hasThumbnail ) {
+
+					$data[ 'src' ]	=	$image->getThumbnailFilename();
+					
+				}
+
+	        }
+
+		}
+
+		//error messages
+		if ( !$isType ) {
+			
+			new Message('error', 'Het bestand moet van het type jpeg, png of gif zijn.');
+            header('location: photo-upload-form.php');
+
+		}
+
+		if ( !$isSize ) {
+			
+			new Message('error', 'Het bestand kleiner zijn dan 2MB.');
+            header('location: photo-upload-form.php');
+
+		}
+
+		if ( $hasError ) {
+
+			new Message('error', 'Er is een fout gebeurd bij het uploaden.');
+            header('location: photo-upload-form.php');
+
+		}
+
+
+
+
+	}
+
+?>	
