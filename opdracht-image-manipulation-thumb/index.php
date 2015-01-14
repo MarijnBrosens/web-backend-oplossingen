744f925053c76f60438850e4d6b725d0a2d5b109
Marijn Brosens <marijnbrosens16@hotmail.com>
2015-01-14 05:54:46 +0100

    opdracht-image-manipulation

---
+++ opdracht-image-manipulation-thumb/index.php
@@ -0,0 +1,29 @@
+<?php
+
+	
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>thumbnail</title>
+    <link rel="stylesheet" href="../style.css">  
+</head>
+<body>
+
+	<h1>thumb</h1>
+
+	<form action="thumb-manipulation-proces.php" method="POST" enctype="multipart/form-data">
+		
+		<input type="file" name="image" id="image">
+
+		<input type="submit" name="submit">
+		
+	</form>
+
+	</body>
+</html>
