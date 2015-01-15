a75150a9b01aabedecb6891c485d37790f2ab8d7
Marijn Brosens <marijnbrosens16@hotmail.com>
2015-01-15 07:54:20 +0100

    opdracht ajax - deel 3 af

--- opdracht-ajax/contact-API.php
+++ opdracht-ajax/contact-API.php
@@ -2,11 +2,15 @@
 
 
 	// Check of de request een ajax-request was
-	if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest')
+	if ( !empty( $_SERVER[ 'HTTP_X_REQUESTED_WITH' ] ) && strtolower( $_SERVER[ 'HTTP_X_REQUESTED_WITH' ] ) == 'xmlhttprequest' )
 	{
+		if ( isset($_POST[ 'email' ] ) )
+		{
 
-		$ajaxMessage['type']	=	'success';
-		echo json_encode($ajaxMessage);
+			$ajaxMessage['type']	=	'success';
+			echo json_encode( $ajaxMessage );
+
+		}
 
 	}
 
